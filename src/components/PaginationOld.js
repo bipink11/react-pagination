b58f96c81052536e345ec9da7bdbf0574608b9e6
Bipin kumar <bipinonkumar@gmail.com>
2020-10-18 17:29:06 +0530

    pagination application demo

---
+++ src/components/PaginationOld.js
@@ -0,0 +1,37 @@
+import React,{useState,useEffect} from 'react'
+
+const  PaginationOld = ({showPerPage,onPaginationChange,total}) => {
+   // console.log("showPerPage = ",showPerPage)
+    const[counter,setCounter] = useState(1);
+    useEffect(() =>{
+        const value = showPerPage*counter;
+        console.log("counter changed....");
+        //console.log("start value ", value - showPerPage);
+        //console.log("end value ", value );
+        onPaginationChange(value - showPerPage,value);
+    },[counter])
+    
+    const onButtonClick = (type) =>{
+        if(type == "prev"){
+            if(counter === 1){
+                setCounter(1);                                                                                                                                                                                                                                                                                                                                   
+            }else{
+                setCounter(counter - 1);
+            }
+        } else if(type == "next"){
+            if(Math.ceil( total/showPerPage) === counter){
+                setCounter(counter); 
+            }else{
+                setCounter(counter + 1);
+            }
+        }
+    }
+    return (
+        <div className="d-flex justify-content-between">
+            <button className="btn btn-primary" onClick={() => onButtonClick("prev")}>Previous</button> 
+            <button className="btn btn-primary" onClick={() => onButtonClick("next")}>Next</button> 
+        </div>
+    )
+}
+
+export default PaginationOld
